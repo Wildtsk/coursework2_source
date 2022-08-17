7fcd3b4a9b6bf68c42494aab943a8273883ab322
Wildtsk <wildtsk@gmail.com>
2022-08-17 12:24:03 +0300

    "исправлены недочеты. Тесты в utils_test.py Правильно ли я все сделала?"

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -3,8 +3,24 @@ import pytest
 from app import app
 
 
-def test_get_posts_by_user():
-    params = {"poster_name": "leo"}
-    response = app.test_client().get("/post/", query_string=params)
+def test_app_read_posts():
+    params = {"s": "python"}
+    response = app.test_client().get('/api/posts', query_string=params)
     print(response.json)
     assert response.status_code == 200
+    assert len(response.json) == 8
+
+
+def test_app_read_post_by_id():
+    params = {"id": 1, "s": "python"}
+    response = app.test_client().get('api/post/<int:id>/', query_string=params)
+    print(response.json)
+    assert response.status_code == 200
+
+
+def test_app_read_post_by_user_name():
+    params = {"id": 1, "user_name": "Molly"}
+    response = app.test_client().get('/api/post/<user_name>/', query_string=params)
+    print(response.json)
+    assert response.status_code == 200
+    assert len(response.json) == 21
